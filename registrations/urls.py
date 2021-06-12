528e481cbf078592da1b47c53be54ae10e3998ea
Pranathi-star <pranathik2001@gmail.com>
2021-06-12 23:08:35 +0530

    worker payment option, resolved the index issue

--- registrations/urls.py
+++ registrations/urls.py
@@ -5,9 +5,10 @@ urlpatterns = [
 	path('partner/', views.register_partner, name="reg_partner"),
 	path('worker/', views.register_worker, name="reg_worker"),
 	path('pricing/', views.pricing, name = "pricing"),
+	path('price_worker/', views.price_worker, name = "price_worker"),
 	path('about_us/', views.about_us, name = "about_us"),
 	path('contact_us/', views.contact_us, name = "contact_us"),
 	path('partners/', views.partners, name = "partners"),
-	path('index/', views.index, name = "index"),
+	path('', views.index, name = "index"),
 	path('join_us/', views.join_us, name = "join_us")
     ]
